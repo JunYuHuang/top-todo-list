cbcfb60d8b229dcc719aee578aa3231998bd7cbb
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-11-15 12:52:42 -0800

    Fix TypeError bugs with Project and Todo classes

--- src/project.js
+++ src/project.js
@@ -1,24 +1,35 @@
 "use strict";
 
 class Project {
+  #id;
+  #name;
+
   constructor(args) {
     const { id, name } = args;
-    this.id = id;
-    this.name = name;
+    this.#id = id;
+    this.#name = name;
   }
 
   get id() {
-    return this.id;
+    return this.#id;
+  }
+
+  set id(value) {
+    this.#id = value;
   }
 
   get name() {
-    return this.name;
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
   }
 
   toPlainObject() {
     return {
-      id: this.id,
-      name: this.name,
+      id: this.#id,
+      name: this.#name,
     };
   }
 }
--- src/todo.js
+++ src/todo.js
@@ -1,67 +1,104 @@
 "use strict";
 
 class Todo {
+  #id;
+  #projectId;
+  #title;
+  #description;
+  #dueDate;
+  #priority;
+  #isDone;
+
   constructor(args) {
     const { id, projectId, title, description, dueDate, priority, isDone } =
       args;
-    this.id = id;
-    this.projectId = projectId;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.isDone = isDone;
+    this.#id = id;
+    this.#projectId = projectId;
+    this.#title = title;
+    this.#description = description;
+    this.#dueDate = dueDate;
+    this.#priority = priority;
+    this.#isDone = isDone;
   }
 
   get id() {
-    return this.id;
+    return this.#id;
+  }
+
+  set id(value) {
+    this.#id = value;
   }
 
   get projectId() {
-    return this.projectId;
+    return this.#projectId;
+  }
+
+  set projectId(value) {
+    this.#projectId = value;
   }
 
   get title() {
-    return this.title;
+    return this.#title;
+  }
+
+  set title(value) {
+    this.#title = value;
   }
 
   get description() {
-    return this.description;
+    return this.#description;
+  }
+
+  set description(value) {
+    this.#description = value;
   }
 
   get dueDate() {
-    return this.dueDate;
+    return this.#dueDate;
+  }
+
+  set dueDate(value) {
+    this.#dueDate = value;
   }
 
   get priority() {
-    return this.priority;
+    return this.#priority;
+  }
+
+  set priority(value) {
+    this.#priority = value;
   }
 
   get isDone() {
-    return this.isDone;
+    return this.#isDone;
+  }
+
+  set isDone(value) {
+    this.#isDone = value;
   }
 
   toPlainObject() {
     return {
-      id: this.id,
-      projectId: this.projectId,
-      title: this.title,
-      description: this.description,
-      dueDate: this.dueDate,
-      priority: this.Priority,
-      isDone: this.isDone,
+      id: this.#id,
+      projectId: this.#projectId,
+      title: this.#title,
+      description: this.#description,
+      dueDate: this.#dueDate,
+      priority: this.#priority,
+      isDone: this.#isDone,
     };
   }
 
   update(options) {
-    if (options.hasOwnProperty("id")) this.id = options.id;
-    if (options.hasOwnProperty("projectId")) this.projectId = options.projectId;
-    if (options.hasOwnProperty("title")) this.title = options.title;
+    if (options.hasOwnProperty("id")) this.#id = options.id;
+    if (options.hasOwnProperty("projectId"))
+      this.#projectId = options.projectId;
+    if (options.hasOwnProperty("title")) this.#title = options.title;
     if (options.hasOwnProperty("description"))
-      this.description = options.description;
-    if (options.hasOwnProperty("dueDate")) this.dueDate = options.dueDate;
-    if (options.hasOwnProperty("priority")) this.priority = options.priority;
-    if (options.hasOwnProperty("isDone")) this.isDone = options.isDone;
+      this.#description = options.description;
+    if (options.hasOwnProperty("dueDate")) this.#dueDate = options.dueDate;
+    if (options.hasOwnProperty("priority")) this.#priority = options.priority;
+    if (options.hasOwnProperty("isDone")) this.#isDone = options.isDone;
   }
 }
 
